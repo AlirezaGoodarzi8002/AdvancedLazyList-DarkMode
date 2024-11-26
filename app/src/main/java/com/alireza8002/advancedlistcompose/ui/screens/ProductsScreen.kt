88958ef41c8f37736d1b2744f729be7c36a49331
Alireza Goodarzi <alireza.goodarzi@securitywave.co>
2024-11-26 18:56:41 +0330

    Add space for end of top bar

--- app/src/main/java/com/alireza8002/advancedlistcompose/ui/screens/ProductsScreen.kt
+++ app/src/main/java/com/alireza8002/advancedlistcompose/ui/screens/ProductsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
@@ -71,6 +72,7 @@ fun ProductsScreen(isDarkTheme: MutableState<Boolean> = mutableStateOf(false)) {
                             .clickable { isDarkTheme.value = !isDarkTheme.value }
                             .size(24.dp)
                     )
+                    Spacer(modifier = Modifier.width(8.dp))
                 }
             )
         },
